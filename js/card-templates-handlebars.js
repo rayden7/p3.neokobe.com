1d5cff07ca597a73178a732720b38043a25dfa95
David Killeffer <rayden7@gmail.com>
2012-11-24 23:38:42 -0500

    first version, still incomplete, "work-in-progress"

---
+++ js/card-templates-handlebars.js
@@ -0,0 +1,181 @@
+<script id="card-template-2" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotB2">{{cardSuitSymbol}}</div>
+            <div class="spotB4">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+</script>
+
+<script id="card-template-3" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotB3">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+    </script>
+
+<script id="card-template-4" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotA5">{{cardSuitSymbol}}</div>
+            <div class="spotC1">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+</script>
+
+<script id="card-template-5" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotA5">{{cardSuitSymbol}}</div>
+            <div class="spotB3">{{cardSuitSymbol}}</div>
+            <div class="spotC1">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+    </script>
+
+
+<script id="card-template-6" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotA3">{{cardSuitSymbol}}</div>
+            <div class="spotA5">{{cardSuitSymbol}}</div>
+            <div class="spotC1">{{cardSuitSymbol}}</div>
+            <div class="spotC3">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+</script>
+
+<script id="card-template-7" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotA3">{{cardSuitSymbol}}</div>
+            <div class="spotA5">{{cardSuitSymbol}}</div>
+            <div class="spotB3">{{cardSuitSymbol}}</div>
+            <div class="spotC1">{{cardSuitSymbol}}</div>
+            <div class="spotC3">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+    </script>
+
+<script id="card-template-8" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotA2">{{cardSuitSymbol}}</div>
+            <div class="spotA4">{{cardSuitSymbol}}</div>
+            <div class="spotA5">{{cardSuitSymbol}}</div>
+            <div class="spotC1">{{cardSuitSymbol}}</div>
+            <div class="spotC2">{{cardSuitSymbol}}</div>
+            <div class="spotC4">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+</script>
+
+<script id="card-template-9" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotA2">{{cardSuitSymbol}}</div>
+            <div class="spotA4">{{cardSuitSymbol}}</div>
+            <div class="spotA5">{{cardSuitSymbol}}</div>
+            <div class="spotB3">{{cardSuitSymbol}}</div>
+            <div class="spotC1">{{cardSuitSymbol}}</div>
+            <div class="spotC2">{{cardSuitSymbol}}</div>
+            <div class="spotC4">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+    </script>
+
+<script id="card-template-10" type="text/x-handlebars-template">
+    <!-- {{cardName}}-->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotA2">{{cardSuitSymbol}}</div>
+            <div class="spotA4">{{cardSuitSymbol}}</div>
+            <div class="spotA5">{{cardSuitSymbol}}</div>
+            <div class="spotB2">{{cardSuitSymbol}}</div>
+            <div class="spotB4">{{cardSuitSymbol}}</div>
+            <div class="spotC1">{{cardSuitSymbol}}</div>
+            <div class="spotC2">{{cardSuitSymbol}}</div>
+            <div class="spotC4">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+</script>
+
+<script id="card-template-J" type="text/x-handlebars-template">
+    <!-- {{cardName}} -->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <img class="face" src="graphics/jack.gif" alt="{{cardName}}" />
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+    </script>
+
+<script id="card-template-Q" type="text/x-handlebars-template">
+    <!-- {{cardName}} -->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <img class="face" src="graphics/queen.gif" alt="{{cardName}}" />
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+</script>
+
+<script id="card-template-K" type="text/x-handlebars-template">
+    <!-- {{cardName}} -->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <img class="face" src="graphics/king.gif" alt="{{cardName}}" />
+            <div class="spotA1">{{cardSuitSymbol}}</div>
+            <div class="spotC5">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+    </script>
+
+<script id="card-template-A" type="text/x-handlebars-template">
+    <!-- {{cardName}} -->
+    <div class="card">
+        <div class="front {{cardColor}}">
+            <div class="index">{{cardRank}}<br />{{cardSuitSymbol}}</div>
+            <div class="spotB3 aceicon">{{cardSuitSymbol}}</div>
+        </div>
+    </div>
+</script>
